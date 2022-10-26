0973734c0547ca398c1de29bb1c3c6723c8cb2ee
Ysquid <ahk648@gmail.com>
2022-10-26 12:09:33 -0600

    comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import App from "./app/App";
 import store from "./app/store";
 
+//comments in the index file to test some git stuff
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
