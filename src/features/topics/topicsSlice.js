0ade8652759e4d04bd1543681e8af87a9d624a0f
Ysquid <ahk648@gmail.com>
2022-10-26 09:47:44 -0600

    topicsSlice done and adding new topic is working

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  topics: {}
+};
+
+export const topicsSlice = createSlice({
+  name: 'topics',
+  //this right?
+  initialState: initialState,
+  reducers: {
+    addTopic: {
+      reducer: (state, action) => {
+        const {id, name, icon} = action.payload
+        state.topics[id] = {
+          id: id,
+          name: name,
+          icon: icon,
+          quizIds: [],
+        }
+
+        }
+      }
+    }
+  }
+)
+
+export const selectTopics = (state) => state.topics.topics
+export const {addTopic} = topicsSlice.actions
+export default topicsSlice.reducer
